262fd738df8c550f2987947268ee985db6063b56
Marcelo Dos Santos <marcelo_mdss@live.com>
2021-12-27 22:38:53 -0300

    Atividades realizadas para aprimoramento

---
+++ URI 1046/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+
+namespace URI_1046
+{
+
+    static class Program
+    {
+        static void Main(String[] args)
+        {
+
+
+            string[] vetnumber = new string[2];
+
+            vetnumber = Console.ReadLine().Split(' ');
+
+
+            int hi = int.Parse(vetnumber[0]);
+            int hf = int.Parse(vetnumber[1]);
+
+            int hr = hf - hi;
+
+            if (hr < 0)
+            {
+                hr = 24 + (hf - hi);
+            }
+
+            if (hf == hi)
+            {
+                Console.WriteLine("O JOGO DUROU 24 HORA(S)");
+            }
+            else
+            {
+                Console.WriteLine("O JOGO DUROU " + hr + " HORA(S)");
+            }
+        }
+    }
+}
---
+++ URI 1047/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace URI_1047
+{
+    static class Program
+    {
+        static void Main(String[] args)
+        {
+            String[] vetnumber = new string[4];
+
+            vetnumber = Console.ReadLine().Split(' ');
+
+            int H_inicial = int.Parse(vetnumber[0]);
+            int M_inicial = int.Parse(vetnumber[1]);
+            int H_final = int.Parse(vetnumber[2]);
+            int M_final = int.Parse(vetnumber[3]);
+
+            int ht, mt;
+
+            if (H_final > H_inicial) 
+            { 
+                ht = H_final - H_inicial;
+                if ()
+                {
+
+                }
+                
+
+                
+            }
+            else
+            {
+                ht = 24 + (H_final - H_inicial);
+            }
+
+            Console.WriteLine(ht);
+
+        }
+    }
+}
